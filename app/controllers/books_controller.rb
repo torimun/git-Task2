5668e621315f2a42c04dbe3f90c016cd3554b8c8
ToriMun <132326048+torimun@users.noreply.github.com>
2023-05-22 10:41:42 +0000

    [Add]top_HTMLの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,19 @@ class BooksController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   def show
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
